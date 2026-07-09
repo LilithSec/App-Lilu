f095eeec1bb1cd7b76d55cad9205dd822eaf641c
Zane C. Bowers-Hadley <vvelox@vvelox.net>
2026-07-09 18:02:08 -0500

    add initial files

---
+++ lib/App/Lilu.pm
@@ -0,0 +1,105 @@
+package App::Lilu;
+
+use 5.006;
+use strict;
+use warnings;
+
+=head1 NAME
+
+App::Lilu - The great new App::Lilu!
+
+=head1 VERSION
+
+Version 0.0.1
+
+=cut
+
+our $VERSION = '0.0.1';
+
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use App::Lilu;
+
+    my $foo = App::Lilu->new();
+    ...
+
+=head1 EXPORT
+
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
+
+=head1 SUBROUTINES/METHODS
+
+=head2 function1
+
+=cut
+
+sub function1 {
+}
+
+=head2 function2
+
+=cut
+
+sub function2 {
+}
+
+=head1 AUTHOR
+
+Zane C. Bowers-Hadley, C<< <vvelox at vvelox.net> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-app-lilu at rt.cpan.org>, or through
+the web interface at L<https://rt.cpan.org/NoAuth/ReportBug.html?Queue=App-Lilu>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc App::Lilu
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker (report bugs here)
+
+L<https://rt.cpan.org/NoAuth/Bugs.html?Dist=App-Lilu>
+
+=item * CPAN Ratings
+
+L<https://cpanratings.perl.org/d/App-Lilu>
+
+=item * Search CPAN
+
+L<https://metacpan.org/release/App-Lilu>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 LICENSE AND COPYRIGHT
+
+This software is Copyright (c) 2026 by Zane C. Bowers-Hadley.
+
+This program is released under the following license:
+
+  agpl
+
+
+=cut
+
+1; # End of App::Lilu
